9db532d4e617b5165b5c962c948cb85de73d4119
Danish Choudhary <46241034+Danish-Choudhary@users.noreply.github.com>
2020-11-09 10:24:37 +0530

    Update contentful.js

--- src/contentful.js
+++ src/contentful.js
@@ -1,5 +1,4 @@
 import {createClient} from 'contentful'
-console.log(process.env)
 export default createClient({
     space:process.env.REACT_APP_API_SPACE,
     accessToken: process.env.REACT_APP_ACCESS_TOKEN
